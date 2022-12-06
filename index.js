e5ae102f39d42b4beebe3e9ef23f7a2d02f7f02b
jewon119 <jewon119@gmail.com>
2022-12-06 17:46:28 +0900

    Add: 에러 미들웨어 vs 일반 미들웨어

--- index.js
+++ index.js
@@ -4,13 +4,21 @@ var morgan = require('morgan')
 const app = express()
 const port = 3000
 
-function logger(req, res, next){ // logger 미들웨어 간단 구현
-    console.log(`i am logger. your request url is "${req.url}"`);
-    next(); // 미들웨어에서는 next 함수 반드시 호출해야함
-}
 
-app.use(logger); // 미들웨어를 추가할 때는 use를 사용
-app.use(morgan('dev')) // 서드파티 로거 미들웨어 사용
+function commonMiddleware(req, res, next){
+    console.log('commonMiddleware');
+    next(new Error('error ouccered'));
+};
+
+function errorMiddleware(err, req, res, next){
+    console.log(`errorMiddleware ${err.message}`);
+    next();
+};
+
+app.use(commonMiddleware);
+app.use(errorMiddleware);
+app.use(morgan('dev'));
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');

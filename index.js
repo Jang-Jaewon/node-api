7e2877f55c55a6dd261251ef7cbe4ea1c1d810c4
jewon119 <jewon119@gmail.com>
2022-12-06 18:29:48 +0900

    Add: json 응답 test

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
 const express = require('express')
-var morgan = require('morgan')
-
+const morgan = require('morgan')
 const app = express()
 const port = 3000
+const users = [
+    {id: 1, name: 'Park', age: 13},
+    {id: 2, name: 'Kim', age: 21},
+    {id: 3, name: 'Jang', age: 19},
+    {id: 4, name: 'Lee', age: 15},
+]
 
-
-function commonMiddleware(req, res, next){
-    console.log('commonMiddleware');
-    next(new Error('error ouccered'));
-};
-
-function errorMiddleware(err, req, res, next){
-    console.log(`errorMiddleware ${err.message}`);
-    next();
-};
-
-app.use(commonMiddleware);
-app.use(errorMiddleware);
 app.use(morgan('dev'));
-
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 })
+
+app.get('/users', (req, res) => {
+    res.json(users)
+  })
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);

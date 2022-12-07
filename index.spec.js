2b0f566549b7f5cb7a0bfc02d8a175d7834cb5d4
jewon119 <jewon119@gmail.com>
2022-12-07 15:15:52 +0900

    Add: 사용자 상세보기 Test 코드 구현

--- index.spec.js
+++ index.spec.js
@@ -31,3 +31,34 @@ describe('GET /users는', ()=> {
         })
     })
 })
+
+
+describe('GET /users/1은', ()=> {
+    describe('성공일 경우', ()=> {
+        it('id가 1인 사용자 객체를 반환한다.', (done)=> {
+            request(app)
+                .get('/users/1')
+                .end((err, res)=> {
+                    res.body.should.be.instanceOf(Object);
+                    res.body.should.have.property('id', 1);
+                    res.body.should.have.property('name', 'Park');
+                    res.body.should.have.property('age', 13);
+                    done();
+            })
+        })
+    })
+    describe('실패일 경우', ()=> {
+        it('id가 숫자가 아닐 경우 400으로 응답한다.', (done)=> {
+            request(app)
+                .get('/users/three')
+                .expect(400)
+                .end(done)
+        });
+        it('id를 찾을 수 없는 경우 404로 응답한다.', (done)=> {
+            request(app)
+                .get('/users/9999')
+                .expect(404)
+                .end(done)
+        });
+    })
+})
